283c8b666d8c808064d514a485bfffa727294604
vda82 <vdryha@gmail.com>
2020-01-13 17:56:04 +0100

    Add project files.

---
+++ HW3Collections/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(0, 10);
+            List<int> list = new List<int>();
+            List<int> list1 = new List<int>();
+            for (int i = 1; i <=n; i++)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("Original list is {0}", string.Join(", ", list));
+
+            while (list.Count > 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+
+                    
+                    if (i%2==0)
+                    {
+                        list.RemoveAt(i);
+                        Console.WriteLine("Modified list is {0}", string.Join(", ", list));
+                    }
+                }
+
+            }
+
+
+
+
+
+        }
+           
+    }
+}
+
---
+++ HW3Collections/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HW3Collections")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("HW3Collections")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("122ef3ca-fa95-44a5-a768-4bb798d83fc0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
